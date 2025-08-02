6f2667bdecfa6d085806a0d880b63df563e1e989
fireonehero <fireonehero+dev@pm.me>
2025-08-01 21:01:23 -0500

    Started

---
+++ build/CMakeFiles/twitch_chat_beep.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/twitch_chat_beep.dir/link.d"
+  "CMakeFiles/twitch_chat_beep.dir/main.cpp.o"
+  "CMakeFiles/twitch_chat_beep.dir/main.cpp.o.d"
+  "twitch_chat_beep"
+  "twitch_chat_beep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/twitch_chat_beep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
